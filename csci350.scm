b8aea4697594550df19a1025ff36c1c1a758104e
Bola Awoyemi <bolaawoyemi@Bolas-MacBook-Air.local>
2020-02-04 23:55:40 -0500

    the rest of Problem 3
    the conclusion to this function

--- csci350.scm
+++ csci350.scm
@@ -40,6 +40,8 @@
 (define (sum-up-numbers-general L)
     (cond
         ((null? L) 0)
-        ((list? (car L)) (+ (sum-up-numbers-general (car L))))
+        ((list? (car L)) (+ (sum-up-numbers-general (car L)) (sum-up-numbers-general (cdr L))))
+        ((not(number? (car L))) (sum-up-numbers-general (cdr L))) 
+        (else (+ (car L) (sum-up-numbers-general (cdr L))))
     )
 )   
