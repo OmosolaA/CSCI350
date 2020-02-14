6af3dc25051b614577e8489156ee74959b626dc2
OmosolaA <omosola.a.awoyemi@gmail.com>
2020-02-13 22:40:45 -0500

    Update csci350.pl

--- csci350.pl
+++ csci350.pl
@@ -1,6 +1,9 @@
-# Problem 1 sum-up-numbers-simple(L, N)
+#Problem 1 sum-up-numbers-simple(L, N)
 sum-up-numbers-simple([],0).
 sum-up-numbers-simple([A|B], N):-
     number(A), 
     sum-up-numbers-simple(B, R), 
     N is B + R
+
+sum-up-numbers-simple([_|B], N):-
+    sum-up-numbers-simple(B, N). 
