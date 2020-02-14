452ed95f0d5b258da634f35ec0a62ecbee9348d6
OmosolaA <omosola.a.awoyemi@gmail.com>
2020-02-13 23:08:55 -0500

    Update csci350.pl

--- csci350.pl
+++ csci350.pl
@@ -7,3 +7,9 @@ sum-up-numbers-simple([A|B], N):-
 
 sum-up-numbers-simple([_|B], N):-
     sum-up-numbers-simple(B, N). 
+    
+#Problem 2 sum-up-numbers-general(L, N)
+sum-up-numbers-general([], 0).
+sum-up-numbers-general([A|B], N):-
+    is_list(A), 
+    sum-up-numbers-general(B, R),
